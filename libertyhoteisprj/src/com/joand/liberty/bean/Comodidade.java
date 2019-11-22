10b4880958f56dac045f03aba337ae34fea58e3d
Anderson Pereira de Lima <944931+andersonplima@users.noreply.github.com>
2019-11-22 19:36:08 -0300

    Comodidade como enum.

--- libertyhoteisprj/src/com/joand/liberty/bean/Comodidade.java
+++ libertyhoteisprj/src/com/joand/liberty/bean/Comodidade.java
@@ -1,28 +1,8 @@
 package com.joand.liberty.bean;
 
-public class Comodidade {
-	private int id;
-	private String descricao;
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Comodidade(int id, String descricao) {
-		super();
-		this.descricao = descricao;
-		this.id = id;
-	}
+public enum Comodidade {
+	ACADEMIA,
+	ESTACIONAMENTO,
+	PISCINA,
+	SINUCA
 }
